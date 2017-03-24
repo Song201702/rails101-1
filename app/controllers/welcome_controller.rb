e3bca33ad02b993c30e47643a4b69907a4d3b026
Chen Song <songminmin999@gmail.com>
2017-03-24 10:38:46 +0800

    create groups index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,13 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "早安！你好！"
+  end
+
+  def index
+    flash[:alert] = "Good night, time to sleep!"
+  end
+
+  def index
+    flash[:warning] = "warning information!"
+  end
 end
